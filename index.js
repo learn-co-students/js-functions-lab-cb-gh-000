d7f8507cb4f78743e976127493616d09075652a8
Saikat-Sinha <saikat@myself.com>
2017-04-21 09:40:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
 // write your code below!
-function happyHolidays(){
-  return "Happy holidays!";
-}
-function happyHolidaysTo(name){
-  return `Happy holidays, ${name}!`;
-}
-function happyHolidayTo(holiday, days){
-  return `It's ${days} days until ${holiday}!`;
-}
-function holidayCountdown(holiday, days){
-  return `It's ${days} days until ${holiday}!`:
-}
+
