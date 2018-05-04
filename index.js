970ed4198618c7417cd684fe6ef45a034a8c2171
Nicholas Liang <ssss021418@gmail.com>
2018-05-04 16:11:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // write your code below!
-var happyHolidays = () => "Happy holidays!"
-var happyHolidaysTo = (name) => "happy holidays, $(name)!"
-var happyHolidaysTo = (holiday, name) => "Happy $(holiday), $(name)!"
-var holidayCountdown = (holiday, days) => "It\'s ${days} days until ${holiday}!"
+var happyHolidays = () => "Happy holidays!";
+var happyHolidaysTo = (name) => 'Happy holidays, ' + name + '!';
+var happyHolidayTo = (holiday, name) => 'Happy ' + holiday + ', ' + name + '!';
+var holidayCountdown = (holiday, days) => 'It\'s '+days+' days until '+holiday+'!'
