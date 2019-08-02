19adf6c471e0a6b77fe6ef8f18e8926cde6641dc
Duong My Phuong <kho.design113@gmail.com>
2019-08-02 00:50:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!";
+}
+function happyHolidaysTo(s) {
+  return `Happy holidays, ${s}!`
+}
+function happyHolidayTo(s,n) {
+  return `Happy ${s}, ${n}!`
+}
+function holidayCountdown(s,n) {
+  return `It's ${n} days until ${s}!`
+}
